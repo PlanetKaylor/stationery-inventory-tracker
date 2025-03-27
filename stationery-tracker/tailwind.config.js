d1c6b116f8eb3e1591b76e4ca2899dbf78c42dbc
Kaylor Davis <74217234+PlanetKaylor@users.noreply.github.com>
2025-03-26 21:36:14 -0700

    Initial Setup
    - setup React component
    - setup Tailwindcss config
    - setup CRACO config

---
+++ stationery-tracker/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./public/index.html", // optional
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
